ff0453cb0b6c3914631e5034bbcaf40f8795073c
unknown <zxckendrick123@gmail.com>
2024-06-19 17:31:19 +0800

    updated right sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
             <div className={nav ? 'z-10 fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500'
             : 'fixed left-[-100%]' }>
-                <h1 className='text-3xl primary-color m-4'>J. DOE</h1>
+                <img src={MyLogo} class="logo px-6 my-10 mx h-[48px] " alt='mylogo' />
                 <ul className='p-8 text-2xl'>
                     <li className='p-2'><a href='#home'>Home</a></li>
                     <li className='p-2'><a href='#about' >About</a></li>
