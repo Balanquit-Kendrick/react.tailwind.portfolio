8fae777d077925dc34443b734b3cd7639fa77de4
unknown <zxckendrick123@gmail.com>
2024-06-25 13:55:04 +0800

    remove projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import proj1 from '../assets/proj1.png'
+import proj1 from '../assets/Movie.png'
 import proj2 from '../assets/Tic-tac-toe.png'
 import proj3 from '../assets/Weather.png'
-import proj4 from '../assets/proj4.png'
-import proj5 from '../assets/proj5.png'
-import proj6 from '../assets/proj6.png';
+
 
 const Work = () => {
   return (
@@ -55,60 +53,9 @@ const Work = () => {
                         <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>View</button>
                     </a>
                 </div>
-            
-            </div>
-
-            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden 
-                            shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center
-                            mx-auto content-div h-[200px] bg-cover relative'>
-                            
-                <img src={proj4} layout="fill" objectFit="cover" alt="" />
-                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
-                    <span className='text-2xl font-bold text-black tracking-wider'>Project</span>
-                    <div className='pt-8 text-center'></div>
-                    <a href="/">
-                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>View</button>
-                    </a>
-                </div>
-            </div>
-
-            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden 
-                            shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center
-                            mx-auto content-div h-[200px] bg-cover relative'>
-                            
-                <img src={proj5} layout="fill" objectFit="cover" alt="" />
-                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
-                    <span className='text-2xl font-bold text-black tracking-wider'>Project</span>
-                    <div className='pt-8 text-center'></div>
-                    <a href="/">
-                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>View</button>
-                    </a>
-                </div>
-            </div>
-
-            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden 
-                            shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center
-                            mx-auto content-div h-[200px] bg-cover relative'>
-                            
-                <img src={proj6} layout="fill" objectFit="cover" alt="" />
-                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
-                    <span className='text-2xl font-bold text-black tracking-wider'>Project</span>
-                    <div className='pt-8 text-center'></div>
-                    <a href="/">
-                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>View</button>
-                    </a>
-                </div>
             </div>
 
         </div>
-
-        
-
-
-
-
-
-
     </div>
   )
 }
