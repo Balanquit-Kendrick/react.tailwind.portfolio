ad03742c282c6b0dfa84d8914731abbf28eef9e8
unknown <zxckendrick123@gmail.com>
2024-11-08 22:25:49 +0800

    added new proj

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import proj1 from '../assets/Movie.png'
 import proj2 from '../assets/Tic-tac-toe.png'
 import proj3 from '../assets/Weather.png'
+import proj4 from '../assets/Todo.png'
 
 
 const Work = () => {
@@ -55,6 +56,19 @@ const Work = () => {
                 </div>
             </div>
 
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden 
+                            shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center
+                            mx-auto content-div h-[200px] bg-cover relative'>
+                            
+                <img src={proj4} layout="fill" objectFit="cover" alt="" />
+                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
+                    <span className='text-2xl font-bold text-black tracking-wider'>Weather App</span>
+                    <div className='pt-8 text-center'></div>
+                    <a href="/weather-app/">
+                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>View</button>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
   )
